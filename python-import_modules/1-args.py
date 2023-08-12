5391cf3f3598eb9b6a7aa3982eb298aec8854bfe
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-08-12 13:02:21 +0100

    sys arguments

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,20 +1,24 @@
-import sys
+def main():
+    import sys
 
-# print('\n')
-# print(sys.argv)
-# print('\n')
+    # print('\n')
+    # print(sys.argv)
+    # print('\n')
 
-# print(len(sys.argv))
+    # print(len(sys.argv))
 
-# print('\n')
+    # print('\n')
 
-no_of_arguments = len(sys.argv)
+    no_of_arguments = len(sys.argv)
 
-if (no_of_arguments == 1):
-    print('{} argument: {}'.format(len(sys.argv), sys.argv))
-    
-elif (no_of_arguments >1):
-    print('{} arguments: {}'.format(len(sys.argv), sys.argv))
-    
-elif (no_of_arguments == 0):
-    print('{} argument.'.format(len(sys.argv)))
+    if (no_of_arguments == 1):
+        print('{} argument: {}'.format(len(sys.argv), sys.argv))
+        
+    elif (no_of_arguments >1):
+        print('{} arguments: {}'.format(len(sys.argv), sys.argv))
+        
+    elif (no_of_arguments == 0):
+        print('{} argument.'.format(len(sys.argv)))
+        
+if __name__ == "__main__":
+    main()

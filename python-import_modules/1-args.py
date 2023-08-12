67d33cdf3970777e0876602a65638c1ab99447c4
Abdullahi <oyelakinabdullahioyedele@gmail.com>
2023-08-12 12:58:50 +0100

    sys arguments

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,20 @@
+import sys
+
+# print('\n')
+# print(sys.argv)
+# print('\n')
+
+# print(len(sys.argv))
+
+# print('\n')
+
+no_of_arguments = len(sys.argv)
+
+if (no_of_arguments == 1):
+    print('{} argument: {}'.format(len(sys.argv), sys.argv))
+    
+elif (no_of_arguments >1):
+    print('{} arguments: {}'.format(len(sys.argv), sys.argv))
+    
+elif (no_of_arguments == 0):
+    print('{} argument.'.format(len(sys.argv)))
